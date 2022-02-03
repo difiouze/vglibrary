6edc0462321019ffc060a0cc1ff46e7b69850141
laurentravix <laurentravix@gmail.com>
2022-02-03 22:43:53 +0100

    feat: Favorite remove button script

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,8 +1,10 @@
 import React from "react";
+import CloseButton from 'react-bootstrap/CloseButton'
 
 const Favorite = ({ addedGame }) => {
   return (
       <div className="favorite-card" style={{ backgroundImage: `url(${addedGame.background_image})` }}>
+        <CloseButton variant="white" onClick={() => console.log('Hello')} />
         <span className="card-title mb-0">{addedGame.name}</span>
       </div>
   );
