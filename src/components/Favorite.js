e1db7b18b5072a1fb3051e558d3a59e096f94227
laurentravix <laurentravix@gmail.com>
2022-02-20 09:44:27 +0100

    fonction de suppression d'un jeu favoris

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,10 +1,16 @@
 import React from "react";
 import CloseButton from 'react-bootstrap/CloseButton'
 
-const Favorite = ({ addedGame }) => {
+const Favorite = ({ addedGame, favorite, setFavorite }) => {
+
+  const removeItem = (addedGame) => {
+    //console.log(addedGame);
+    setFavorite(favorite.filter((el) => el.id !== addedGame.id));
+  }  
+
   return (
       <div className="favorite-card" style={{ backgroundImage: `url(${addedGame.background_image})` }}>
-        <CloseButton variant="white" onClick={() => console.log('Hello')} />
+        <CloseButton variant="white" onClick={() => removeItem(addedGame)} />
         <span className="card-title mb-0">{addedGame.name}</span>
       </div>
   );
--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Favorite from './Favorite';
 
-const Favorites = ({ favorite }) => {
+const Favorites = ({ favorite, setFavorite }) => {
   return (
     <div className='favorites-container'>
     <h2 className='favorites-title'>Favorite Games saved: {favorite.length}</h2>
     <div className='favorites-item-container'>
     {favorite.map((addedGame) => (
-            <Favorite addedGame= {addedGame} key={addedGame.id}/>
+            <Favorite addedGame= {addedGame} key={addedGame.id} favorite={favorite} setFavorite={setFavorite}/>
         ))}
     </div>
     </div>
